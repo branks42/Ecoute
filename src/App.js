6d45144842251bb5730b6c3784e4f4fb37b1802f
Justin Branco <jbranco@nd.edu>
2020-04-13 10:39:13 -0400

    Ecoute initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./images/ecoute.png";
+import "./App.css";
+import SpotifyPlayer from 'react-spotify-web-playback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="logo" width="400" height='156' />
+        </header>
+        <SpotifyPlayer
+          token='BQAJCmtjdc7GIEXoWgGiCGpZU1FEufzR5A9mRczo0wnA55S2W7CabXlxSvganmXLQ4nd_z8AKEO2pKvRObMrLjRvuvLVINgnNVkbEOqeklKOjxnULKvv_fCdRr0ifmpTGlwrJN3nddhuUsrkfmVs_TrzEusUCXqhW38LQ5vQWU7r_hZcT0K1oDopZsIS7nLOfkOKrHE-bK4A9SY'
+          uris={['spotify:track:7pQSwujyYbzU35XZeXLpOc']}
+          name='Ecoute Web Player'
+        />
+      </div>
+    </Fragment>
   );
 }
 
