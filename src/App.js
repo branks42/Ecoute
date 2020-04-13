0556a503246f157a4e84443a60ddf62e7a8f3e04
Justin Branco <jbranco@nd.edu>
2020-04-13 10:40:33 -0400

    teehee

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <img src={logo} alt="logo" width="400" height='156' />
         </header>
         <SpotifyPlayer
-          token='BQAJCmtjdc7GIEXoWgGiCGpZU1FEufzR5A9mRczo0wnA55S2W7CabXlxSvganmXLQ4nd_z8AKEO2pKvRObMrLjRvuvLVINgnNVkbEOqeklKOjxnULKvv_fCdRr0ifmpTGlwrJN3nddhuUsrkfmVs_TrzEusUCXqhW38LQ5vQWU7r_hZcT0K1oDopZsIS7nLOfkOKrHE-bK4A9SY'
+          token=''
           uris={['spotify:track:7pQSwujyYbzU35XZeXLpOc']}
           name='Ecoute Web Player'
         />
